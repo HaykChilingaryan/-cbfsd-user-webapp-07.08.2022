4a4e3089d8bdecd0c7fbee08776b00ec6f03d548
Hayk Chilingaryan <haykchilingaryan2002@gmail.com>
2022-08-07 09:26:36 -0700

    Added Initial Project Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductsModule } from './components/products/products.module';
+import { AuthModule } from './components/auth/auth.module';
+import { OrdersModule } from './components/orders/orders.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule,
+    AuthModule,
+    OrdersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
